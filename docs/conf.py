e1944c5a3a581608b0de317a0be9a6a981e6b4c7
Fabian Ball <fabian.ball@kit.edu>
2018-08-29 17:53:10 +0200

    Fixed docs

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,8 @@ extensions = [
 	'sphinx.ext.doctest',
 	'sphinx.ext.autodoc',
 	'sphinx.ext.coverage',
-	'sphinx.ext.viewcode']
+	'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
